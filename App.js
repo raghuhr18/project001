aff23b4df1974a9410871d20e317196096781ee1
raghuhr18 <raghuhr18@yahoo.com>
2023-08-03 23:39:08 +0530

    set up

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading1 = React.createElement("h1",{id:"container"},"Heading");
-const root2 = ReactDOM.createRoot(document.getElementById("root"));
-root2.render(heading1);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading1);
 
