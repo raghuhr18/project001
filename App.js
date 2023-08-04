456e38de8ad685c74fa1c4cddf68366f03b3427a
raghuhr18 <raghuhr18@yahoo.com>
2023-08-04 23:45:06 +0530

    passing props

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const heading1 = React.createElement("h1",{id:"container"},"Heading");
+
+const heading2 = <h1 id='title' key="h2" className="heading">Heading</h1>
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading1);
+root.render(heading2);
 
